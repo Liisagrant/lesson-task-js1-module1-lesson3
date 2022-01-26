81f0dcf1227cecae0deabd86973517b7c945da13
Liisagrant <lisagrant1990@gmail.com>
2022-01-26 19:22:04 +0100

    stared

--- js/products.js
+++ js/products.js
@@ -14,3 +14,21 @@ const products = [
         soldOut: true,
     },
 ];
+
+const container = document.querySelector(".container");
+console.log(container);
+
+const myProductName = product.name;
+const MyProductPrice = product.price;
+const IsMyProductSoldOut = product.soldOut ? "" : "";
+
+container.innerHTML = `<h4 style ="`
+
+
+
+
+//for (let i = 0; i < products.length; i++) {
+   // console.log(products[i]);
+//}
+
+
