d01e059cf1239298b9e582dc57bd2fc88bbf0643
Liisagrant <lisagrant1990@gmail.com>
2022-01-27 11:10:39 +0100

    did the last task for yesterday

--- js/product.js
+++ js/product.js
@@ -3,3 +3,30 @@ const product = {
     price: 99,
     onSale: true,
 };
+
+
+
+let html = "";
+
+const container = document.querySelector(".container");
+
+let seePrice = "Unavailable";
+
+if(product.price) {
+    seePrice = product.price;
+}
+
+let textDeco = "none";
+
+if (product.onSale) {
+    textDeco = "underline";
+}
+
+container.innerHTML = `
+<div class = "product">
+<h2 style = "text-decoration: ${textDeco}">Name: ${product.name}<h2>
+<h3>Price: ${seePrice}<h3>
+</div>`;
+
+
+console.log(html);
